183d9e61f056b871d891cf5b65aee575d1371a1f
Cesar Martinez <oliver-cesar@hotmail.com>
2021-08-19 11:41:17 -0600

    Ejercicio asignado en Clase Practica Assemblies C#.

---
+++ LibraryMonth/NumberMonth.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryMonth
+{
+    public class NumberMonth
+    {
+        public static void Month()
+        {
+            Console.WriteLine("Ingrese un numero del 1 al 12, el cual representara el mes que correspone al numero");
+            var numberMonth = Convert.ToInt32(Console.ReadLine());
+
+            switch (numberMonth)
+            {
+                case 1:
+                    Console.WriteLine("El numero {0}, corresponde al mes de: Enero.", numberMonth);
+                    break;
+                case 2:
+                    Console.WriteLine("El numero {0}, corresponde al mes de: Febrero.", numberMonth);
+                    break;
+                case 3:
+                    Console.WriteLine("El numero {0}, corresponde al mes de: Marzo.", numberMonth);
+                    break;
+                case 4:
+                    Console.WriteLine("El numero {0}, corresponde al mes de: Abril.", numberMonth);
+                    break;
+                case 5:
+                    Console.WriteLine("El numero {0}, corresponde al mes de: Mayo.", numberMonth);
+                    break;
+                case 6:
+                    Console.WriteLine("El numero {0}, corresponde al mes de: Junio.", numberMonth);
+                    break;
+                case 7:
+                    Console.WriteLine("El numero {0}, corresponde al mes de: Julio.", numberMonth);
+                    break;
+                case 8:
+                    Console.WriteLine("El numero {0}, corresponde al mes de: Agosto.", numberMonth);
+                    break;
+                case 9:
+                    Console.WriteLine("El numero {0}, corresponde al mes de: Septiembre.", numberMonth);
+                    break;
+                case 10:
+                    Console.WriteLine("El numero {0}, corresponde al mes de: Octubre.", numberMonth);
+                    break;
+                case 11:
+                    Console.WriteLine("El numero {0}, corresponde al mes de: Noviembre.", numberMonth);
+                    break;
+                case 12:
+                    Console.WriteLine("El numero {0}, corresponde al mes de: Diciembre.", numberMonth);
+                    break;
+                default:
+                    Console.WriteLine("El numero ingresado, no corresponde a uno de los 12 meses del año.");
+                    break;
+            }
+
+        }
+    }
+}
---
+++ UseLibraryNumberMonth/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibraryMonth;
+
+namespace UseLibraryNumberMonth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NumberMonth.Month();
+            Console.ReadKey();
+        }
+    }
+}
